f467528abd0334d76401d0508fea5a2fa0a05cd9
ArditShef <as41519@ubt-uni.net>
2022-07-27 21:37:33 +0200

    hello

--- utils/scroll-observer.tsx
+++ utils/scroll-observer.tsx
@@ -1,1 +1,17 @@
 import React, { useCallback, useEffect, useState } from "react";
+
+interface ScrollValue {
+    scrollY: number
+}
+
+export const ScrollContext = React.createContext<ScrollValue>({
+    scrollY: 0
+})
+
+const ScrollObserver: React.FC = ({ children }) => {
+    const [scrollY, setScrollY] = useState(0)
+    const handleScroll = useCallback(() => {
+
+    }, [])
+}
+//
