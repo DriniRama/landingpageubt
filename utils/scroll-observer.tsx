2e3014e2d577b75f765f3ce641314371b61e4345
pixendrit <pixendrit@outlook.com>
2022-08-17 03:21:03 +0200

    masthead

--- utils/scroll-observer.tsx
+++ utils/scroll-observer.tsx
@@ -1,29 +1,29 @@
-import React, { Children, useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 interface ScrollValue {
-    scrollY: number
+  scrollY: number
 }
 
 export const ScrollContext = React.createContext<ScrollValue>({
-    scrollY: 0
+  scrollY: 0
 })
 
 const ScrollObserver: React.FC = ({ children }) => {
-    const [scrollY, setScrollY] = useState(0)
-    const handleScroll = useCallback(() => {
-        setScrollY(window.scrollY)
-    }, [])
+  const [scrollY, setScrollY] = useState(0)
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY)
+  }, [])
 
-    useEffect(() => {
-        document.addEventListener('scroll', handleScroll, { passive: true })
-        return () => document.removeEventListener('scroll', handleScroll)
-    }, [handleScroll])
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll, { passive: true })
+    return () => document.removeEventListener('scroll', handleScroll)
+  }, [handleScroll])
 
-    return (
-        <ScrollContext.Provider value={{ scrollY }}>
-            {children}
-        </ScrollContext.Provider>
-    )
+  return (
+    <ScrollContext.Provider value={{ scrollY }}>
+      {children}
+    </ScrollContext.Provider>
+  )
 }
 
 export default ScrollObserver
