a3d80b1d6b9baf11f67d4026f14ae37f7333135f
endritubt <eh41767@ubt-uni.net>
2022-08-25 03:50:00 +0200

    work left

--- components/work.tsx
+++ components/work.tsx
@@ -11,5 +11,18 @@ export const WorkBackground: React.FC = () => (
     <div className="bg-white h-[70vh] lg:min-h-screen"></div>
   </div>
 );
+
+
+export const WorkLeft: React.FC<{ progress: number, children: any }> = ({ children, progress }) => {
+  let translateY = Math.max(0, 50 - progress * 3 * 50)
+  if (progress > 0.85) translateY = Math.max(-50, -(progress - 0.85) * 2 * 50)
+  return (
+    <div className="flex flex-col items-center justify-center text-3xl lg:text-3xl h-[30vh] lg:h-auto"
+      style={{ transform: `translateY(${translateY}px)` }}>
+      <div className="leading-10">{children}</div>
+    </div>
+  )
+}
+
   )
 }
