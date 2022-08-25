adbb5e7a62beb060b182ea87e12aee8abdc08c6b
bleonab <bb48675@ubt-uni.net>
2022-08-26 00:49:59 +0200

    spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const Footer: React.FC = () => (
-  <footer className=" flex grap-8 items-center justify-center bg-black text-white p-20">
+  <footer className=" flex gap-8 items-center justify-center bg-black text-white p-20">
     <Image src={"/assets/ubt-logo.png"} width={18} height={18} alt="logo" />
     <Link href="/terms">Terms</Link>
     <Link href="/privacy">Privacy Policy</Link>
